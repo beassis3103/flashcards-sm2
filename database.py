02c5467f18d688eb1c0b962c471932e3282f7b6a
beassis3103 <biasiqueira602@gmail.com>
2026-07-01 15:46:44 -0300

    criação das tabelas de flashcards e pastas no db

---
+++ database.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+def conectar():
+    conn = sqlite3.connect("flashcards.db")
+    conn.execute("PRAGMA foreign_keys = ON")
+    return conn
+
+def criar_tabelas(conn):
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS pastas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome VARCHAR (100) NOT NULL,
+            dt_criacao DATE DEFAULT CURRENT_DATE
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS flashcards (
+            id INTEGER PRIMARY KEY AUTO INCREMENT,
+            id_pasta INTEGER NOT NULL,
+            pergunta VARCHAR(400) NOT NULL,
+            resposta VARCHAR(400) NOT NULL,
+            intervalo INTEGER DEFAULT = 0,
+            repeticoes INTEGER DEFAULT = 0,
+            ft_facil REAL DEFAULT = 2.5,
+            prox_rev DATE DEFAULT CURRENT_DATE,
+            err_seg INTEGER DEFAULT = 0
+            FOREIGN KEY (id_pasta) REFERENCES pastas(id) ON DELETE CASCADE
+        )
+    """)
+
+    conn.commit()
