8a35e185d8e22f18539fc86b60ead0d888c6879a
beassis3103 <biasiqueira602@gmail.com>
2026-07-31 16:21:57 -0300

    feat: ajuste na função de ajustar qualidade de acordo com o tempo

--- algorithm.py
+++ algorithm.py
@@ -33,4 +33,14 @@ def calcula_proxima_revisao(qualidade, repeticoes, intervalo, ft_facil):
     #Calcula a próxima data de acordo com a nova variação do intervalo
     prox_data = date.today() + timedelta(days = intervalo)
     
+    return intervalo, repeticoes, ft_facil, prox_data
+
+def ajustar_qualidade_tempo(qualidade, tmp_sec):
+    if qualidade >= 3:
+        if tmp_sec > 20:
+            qualidade -= 2
+        elif tmp_sec > 10:
+            qualidade -= 1
+    if qualidade < 3:
+        qualidade = 3
     return qualidade
